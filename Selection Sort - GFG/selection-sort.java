819baa387b18c03f5ab6525602dd8f9a8bf50857
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-11-08 20:09:44 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.java
+++ Selection Sort - GFG/selection-sort.java
@@ -33,30 +33,30 @@ class GFG
 
 class Solution
 {
-     void swap(int[] arr,int i,int j)
-	{
-	    int temp=arr[i];
-	    arr[i]=arr[j];
-	    arr[j]=temp;
-	}
+//      void swap(int[] arr,int i,int j)
+// 	{
+// 	    int temp=arr[i];
+// 	    arr[i]=arr[j];
+// 	    arr[j]=temp;
+// 	}
 	
 	void selectionSort(int arr[], int n)
 	{
-	    for(int i=0; i<n; i++){
-	        int minIdx = i;
+	   // for(int i=0; i<n; i++){
+	   //     int minIdx = i;
 	        
-	        for(int j=i+1; j<n; j++){
-	            if(arr[j] < arr[minIdx]){
-	                minIdx = j;
-	            }
-	        }
-            //swap
-            // int temp=arr[i];
-            // arr[i]=arr[minIdx];
-            // arr[minIdx]=temp;
-            swap(arr,i,minIdx);
-	    }
-	   //Arrays.sort(arr);
+	   //     for(int j=i+1; j<n; j++){
+	   //         if(arr[j] < arr[minIdx]){
+	   //             minIdx = j;
+	   //         }
+	   //     }
+    //         //swap
+    //         // int temp=arr[i];
+    //         // arr[i]=arr[minIdx];
+    //         // arr[minIdx]=temp;
+    //         swap(arr,i,minIdx);
+	   // }
+	   Arrays.sort(arr);
 	    
 	    
 	}
